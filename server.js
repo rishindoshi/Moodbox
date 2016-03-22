2e2568aea91caf1ce8247ed3d6b53b0b6cc93e3a
Dan Wilson <wilsoda@umich.edu>
2016-03-22 00:40:26 -0400

    implement spotify auth

--- server.js
+++ server.js
@@ -2,11 +2,28 @@
 var express = require('express');
 var request = require('request');
 var handles = require('express-handlebars');
-
+var passport = require('passport');
+var session = require('express-session');
 var app = express();
 
+app.use(session({ secret: 'keyboard cat' }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+require('./app/auth')(app, passport);
+
 // Static Files
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 //This allows us to use handlebars as our template engine
 app.set('views', __dirname + '/public/views');
@@ -18,63 +35,35 @@ app.engine('.hbs', handles({
 app.set('view engine', '.hbs');
 
 //Rendering our index page
-app.get('/', function(req, res){
-	res.render('home', {message: 'SOUNDZAY'});
+app.get('/login', function(req, res){
+	res.render('login');
 });
+
+songData = {
+	'song' : [
+		{
+			title: 'Junk of the heart',
+			imgUrl: 'https://facebook.com'
+		},
+		{
+			title: 'Hello',
+			imgUrl: 'Okay'
+		}
+	]
+}
 
 //When a user submits an artist
-app.get('/tracks', function(req, res){
-	//Here we're getting the artist name that the user submitted. We get it from parsing the url.
-	var search_artist_name = req.query.artist_name;
+app.get('/results', loggedIn, function(req, res){
 
-	//These two objects hava data that we will send to Spotify in our request
-	//Spotify uses this request data to determine what data they want to send back to us
-	var artist_req_options = {
-		url: 'https://api.spotify.com/v1/search',
-		qs: {q: search_artist_name, type: 'artist'},
-		limit: 10,
-		method: 'GET'
-	};
-	var tracks_req_options = {
-		url: 'https://api.spotify.com/v1/artists/',
-		qs: {country: 'US'},
-		method: 'GET'
-	};
+	res.render('results', songData);
 
-	//Make our first request to spotify to get the unique ID for the artist that the user submitted
-	request(artist_req_options, function(error, response, body){
-		//Spotify gives us back the data in a format javascript can't understand.
-		//Javascript understands JSON, so we use the JSON.parse function to convert the data
-		var data = JSON.parse(body);
-
-		//UNCOMMENT ME: to see how the data returned by spotify is formatted
-		//console.log(data);
-
-		//We extract the unique artist id and append it to our url for the next request
-		var search_artist_id = data.artists.items[0].id;
-		tracks_req_options.url += (search_artist_id + '/top-tracks');
-
-		//Now that we have the unique artist id, we can request that Artist's top-tracks from Spotify
-		request(tracks_req_options, function(error, response, body){
-			var tracks = JSON.parse(body).tracks;
-	     	var popular_tracks = [];
-
-	     	//iterate through Spotify data and extract what fields we want
-	     	//In this case, we're interestd in the name of the track, a link to the
-	     	//30 second preview, and a link to the image of its album cover
-	     	for(var i=0; i<tracks.length; ++i){
-	     		popular_tracks.push({
-	     			name: tracks[i].name,
-	     			preview: tracks[i].preview_url,
-	     			imageURL: tracks[i].album.images[0].url
-	     		});
-	     	}
-
-	     	//Return a new HTML page to display to the other and also give the above data we extraced
-			res.render('tracks', {name: search_artist_name, tracks: popular_tracks});
-		});
-	});
 });
+
+app.get('/', function(req, res) {
+	console.log(req.user)
+	res.render('home', {user: req.user});
+});
+
 
 //Start the server
 app.listen(8888);
