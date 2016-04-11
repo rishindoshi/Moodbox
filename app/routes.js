a9ab694322d1021b8010cfa988bc9ce992411819
Dan Wilson <wilsoda@umich.edu>
2016-04-11 15:41:56 -0400

    added routes and 5star libs

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,11 @@ module.exports = function(app, api, generate, qgen) {
 
 	// Logged in check
 	function loggedIn(req, res, next) {
-	    if (req.user) {
-	        next();
-	    } else {
-	        res.redirect('/login');
-	    }
+		if (req.user) {
+			next();
+		} else {
+			res.redirect('/login');
+		}
 	}
 
 	//Rendering our index page
@@ -30,31 +30,54 @@ module.exports = function(app, api, generate, qgen) {
 		res.send("todo later");
 	});
 
+	app.get('/results', function(req, res) {
+		sample_data = {
+			tracks : [
+				{
+					img: 'url(https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png)',
+					title: 'Song 1',
+					url: 'this'
+				},
+				{
+					img: 'url(https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png)',
+					title: 'Song 2',
+					url: 'this'
+				},
+				{
+					img: 'url(https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png)',
+					title: 'Song 3',
+					url: 'there'
+				}
+			]
+		};
+			res.render('results', sample_data);
+		});
+
 	app.get('/test', function(req, res){
 		var mood = "happy";
 		var userid = req.query.id;
 		var userArtists = [];
 		generate.getArtists(userid, api)
-			.then(function(aids){
-				userArtists = aids;
-				return generate.getMoodArtists(mood, api);
-			})
-			.then(function(moodArtists){
-				var allArtists = moodArtists.filter(function(n){
-				    return userArtists.indexOf(n) != -1;
-				});
-				return generate.generateTracks(allArtists, api);
-			})
-			.then(function(trackids){
-				console.log(trackids);
-				return generate.makePlaylist(trackids, userid, api);
-			})
-			.then(function(playlist){
-				console.log("SUCCESS CREATING PLAYLIST");
-			})
-			.catch(function(error){
-				console.log(error);
+		.then(function(aids){
+			userArtists = aids;
+			return generate.getMoodArtists(mood, api);
+		})
+		.then(function(moodArtists){
+			var allArtists = moodArtists.filter(function(n){
+				return userArtists.indexOf(n) != -1;
 			});
+			return generate.generateTracks(allArtists, api);
+		})
+		.then(function(trackids){
+			console.log(trackids);
+			return generate.makePlaylist(trackids, userid, api);
+		})
+		.then(function(playlist){
+			console.log("SUCCESS CREATING PLAYLIST");
+		})
+		.catch(function(error){
+			console.log(error);
+		});
 	});
 
 	app.get('/', loggedIn, function(req, res) {
