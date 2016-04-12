bd759afd88747435ca37b4fd2426bcc8dfd8f0aa
Dan Wilson <wilsoda@umich.edu>
2016-04-11 22:51:10 -0400

    added mysql db connection

--- app/db.js
+++ app/db.js
@@ -1,6 +1,7 @@
+// DB config
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-	host     : 'localhost',
+	host     : '104.236.24.198',
 	user     : 'root',
 	password : 'systems2016',
 	database : 'moodbox'
@@ -8,14 +9,4 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-exports = connection;
-
-// connection.query('SELECT * from < table name >', function(err, rows, fields) {
-//   if (!err)
-//     console.log('The solution is: ', rows);
-//   else
-//     console.log('Error while performing Query.');
-// });
-
-// connection.end();
-
+module.exports = connection;
--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,5 @@
 var Q = require('q');
-module.exports = function(app, api, generate, qgen) {
+module.exports = function(app, api, generate, qgen, db) {
 
 	// Logged in check
 	function loggedIn(req, res, next) {
@@ -33,56 +33,56 @@ module.exports = function(app, api, generate, qgen) {
 	app.get('/results', function(req, res) {
 		sample_data = {
 			tracks : [
-				{
-					img: 'url(https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png)',
-					title: 'Song 1',
-					url: 'this'
-				},
-				{
-					img: 'url(https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png)',
-					title: 'Song 2',
-					url: 'this'
-				},
-				{
-					img: 'url(https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png)',
-					title: 'Song 3',
-					url: 'there'
-				}
+			{
+				img: 'url(https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png)',
+				title: 'Song 1',
+				url: 'this'
+			},
+			{
+				img: 'url(https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png)',
+				title: 'Song 2',
+				url: 'this'
+			},
+			{
+				img: 'url(https://a-v2.sndcdn.com/assets/images/header/cloud@2x-e5fba4.png)',
+				title: 'Song 3',
+				url: 'there'
+			}
 			]
 		};
-			res.render('results', sample_data);
-		});
+		res.render('results', sample_data);
+	});
 
 	app.get('/test', function(req, res){
 		var mood = "sunny happy";
 		var userid = req.query.id;
 		var userArtists = [];
 		generate.getArtists(userid, api)
-			.then(function(aids){
-				console.log(aids.length + " user artists");
-				userArtists = aids;
-				return generate.getMoodArtists(mood, api);
-			})
-			.then(function(moodArtists){
-				console.log(moodArtists.length + " mood artists");
-				var allArtists = moodArtists.filter(function(n){
-				    return userArtists.indexOf(n) != -1;
-				});
-				console.log(allArtists.length + " intersection artists");
-				return generate.generateTracks(allArtists, api);
-			})
-			.then(function(trackids){
-				console.log(trackids.length + " tracks");
-				numTracks = (trackids.length < 50) ? trackids.length : 50;
-				trackids = generate.shuffle(trackids).slice(0, numTracks);
-				return generate.makePlaylist(trackids, userid, api);
-			})
-			.then(function(playlist){
-				console.log("SUCCESS CREATING PLAYLIST");
-			})
-			.catch(function(error){
-				console.log(error);
+		.then(function(aids){
+			console.log(aids.length + " user artists");
+			userArtists = aids;
+			return generate.getMoodArtists(mood, api);
+		})
+		.then(function(moodArtists){
+			console.log(moodArtists.length + " mood artists");
+			var allArtists = moodArtists.filter(function(n){
+				return userArtists.indexOf(n) != -1;
 			});
+			console.log(allArtists.length + " intersection artists");
+			return generate.generateTracks(allArtists, api);
+		})
+		.then(function(trackids){
+			console.log(trackids.length + " tracks");
+			numTracks = (trackids.length < 50) ? trackids.length : 50;
+			trackids = generate.shuffle(trackids).slice(0, numTracks);
+			return generate.makePlaylist(trackids, userid, api);
+		})
+		.then(function(playlist){
+			console.log("SUCCESS CREATING PLAYLIST");
+		})
+		.catch(function(error){
+			console.log(error);
+		});
 	});
 
 	app.get('/', loggedIn, function(req, res) {
@@ -96,8 +96,20 @@ module.exports = function(app, api, generate, qgen) {
 	// Rating Route
 	app.post('/rating', function(req, res) {
 		console.log(req.body.rating);
+		var rating = req.body.rating;
+		var emotion = req.body.emotion || "NULL";
+		var playlistID = req.body.playlistID || "0";
+		var query = 'INSERT INTO feedback (rating, emotion, playlistID) VALUES (' + rating + ',"' + emotion +'",' + playlistID + ');';
+		console.log(query);
 		// Save rating in DB
-		res.status(200).end();
+		db.query(query, function(err, rows, fields) {
+			if (!err)
+				console.log('result: ', rows);
+			else
+				console.log('err: ', err);
+		});
+
+		res.send(200);
 	});
 
 	// app.get('*', loggedIn, function(req, res) {
