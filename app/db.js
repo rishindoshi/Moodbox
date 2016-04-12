8e3ec5145286c55898097ae6b57774a4d8d08998
Dan Wilson <wilsoda@umich.edu>
2016-04-11 21:25:43 -0400

    added db config

---
+++ app/db.js
@@ -0,0 +1,21 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'systems2016',
+	database : 'moodbox'
+});
+
+connection.connect();
+
+exports = connection;
+
+// connection.query('SELECT * from < table name >', function(err, rows, fields) {
+//   if (!err)
+//     console.log('The solution is: ', rows);
+//   else
+//     console.log('Error while performing Query.');
+// });
+
+// connection.end();
+
