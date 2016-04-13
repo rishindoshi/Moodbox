e5e2a2f7a9eeec1aa19537d1cea82ac678de8a70
Rishin Doshi <rishdosh@umich.edu>
2016-04-13 17:34:37 -0400

    took some questions out of qgen

--- app/qgen.js
+++ app/qgen.js
@@ -2,11 +2,7 @@ module.exports = function() {
 	questions = [
 		"How was your day today",
 		"How are you feeling",
-		"What is the weather like",
 		"What did you do today",
-		"What was your favorite part about the day",
-		"Why does the sun rise"
-
 	]
 	var num = Math.floor((Math.random() * 1000) % questions.length)
 	return questions[num] + "?";
